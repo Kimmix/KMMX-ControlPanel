a38f9b094e864d9b1a6dd1723c49c354470b2ae5
Kimmix <kimmix05@gmail.com>
2023-08-10 21:26:32 +0700

    adjust gyro

--- app.js
+++ app.js
@@ -110,8 +110,8 @@ if (typeof DeviceOrientationEvent.requestPermission === "function") {
 
 // Function to handle device orientation changes
 function handleOrientation(event) {
-    xRotation = event.beta; // x-axis rotation (tilt forward/backward)
-    yRotation = event.gamma; // y-axis rotation (tilt left/right)
+    xRotation = event.beta * 0.1; // x-axis rotation (tilt forward/backward)
+    yRotation = event.gamma * 0.7; // y-axis rotation (tilt left/right)
 
     // Update element's position using CSS translate
     element.style.transform = "translate(" + yRotation + "px, " + xRotation + "px)";
