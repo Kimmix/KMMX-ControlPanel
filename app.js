a031fb08f6a150a6da4011750dda979b4d1b62a7
Kimmix <kimmix05@gmail.com>
2023-08-11 01:31:57 +0700

    drag noise

--- app.js
+++ app.js
@@ -33,6 +33,38 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// ------- Gyroscope ---------
+// Get the moving element
+var element = document.getElementById("movingElement");
+
+// Initialize variables to store gyroscope data
+var xRotation = 0;
+var yRotation = 0;
+
+// Request permission to access device orientation
+if (typeof DeviceOrientationEvent.requestPermission === "function") {
+    DeviceOrientationEvent.requestPermission()
+        .then(permissionState => {
+            if (permissionState === "granted") {
+                window.addEventListener("deviceorientation", handleOrientation);
+            }
+        })
+        .catch(console.error);
+}
+
+// Function to handle device orientation changes
+function handleOrientation(event) {
+    xRotation = clamp((event.beta - 20) * 0.4, -70, 30); // x-axis rotation (tilt forward/backward)
+    yRotation = clamp(event.gamma * 0.6, -100, 20); // y-axis rotation (tilt left/right)
+
+    // Update element's position using CSS translate
+    element.style.transform = "translate(" + yRotation + "px, " + xRotation + "px)";
+}
+
+function clamp(value, min, max) {
+    return Math.min(Math.max(value, min), max);
+}
+
 // Count Expression button
 var expBtnCount = document.getElementsByClassName("exp-btn").length;
 document.getElementById("expBtnCount").textContent = expBtnCount;
@@ -86,34 +118,57 @@ toggleViseme.addEventListener('click', () => {
     toggleViseme.classList.toggle('active');
 });
 
-// ------- Gyroscope ---------
-// Get the moving element
-var element = document.getElementById("movingElement");
+// Drag control
+const noiseThreshold = document.getElementById('noiseThreshold');
+let dragging = false;
+let currentValue = 100;
 
-// Initialize variables to store gyroscope data
-var xRotation = 0;
-var yRotation = 0;
+noiseThreshold.textContent = formatNumber(currentValue);
 
-// Request permission to access device orientation
-if (typeof DeviceOrientationEvent.requestPermission === "function") {
-    DeviceOrientationEvent.requestPermission()
-        .then(permissionState => {
-            if (permissionState === "granted") {
-                window.addEventListener("deviceorientation", handleOrientation);
-            }
-        })
-        .catch(console.error);
+// Debounce function to delay the API call until a quiet period has passed
+function debounce(func, delay) {
+    let timeoutId;
+    return function (...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            func.apply(this, args);
+        }, delay);
+    };
 }
 
-// Function to handle device orientation changes
-function handleOrientation(event) {
-    xRotation = clamp((event.beta - 20) * 0.4, -70, 30); // x-axis rotation (tilt forward/backward)
-    yRotation = clamp(event.gamma * 0.6, -100, 20); // y-axis rotation (tilt left/right)
+const debouncedApiCall = debounce(sendApiUpdate, 500); // Wait for 500ms of quiet period
 
-    // Update element's position using CSS translate
-    element.style.transform = "translate(" + yRotation + "px, " + xRotation + "px)";
+
+noiseThreshold.addEventListener('mousedown', () => {
+    dragging = true;
+});
+
+document.addEventListener('mouseup', () => {
+    dragging = false;
+    debouncedApiCall(currentValue);
+});
+
+document.addEventListener('mousemove', (event) => {
+    if (dragging) {
+        const deltaY = event.movementY;
+        currentValue -= deltaY * 100;
+
+        // Ensure the value stays within the desired range
+        currentValue = Math.min(5000, Math.max(100, currentValue));
+
+        noiseThreshold.textContent = formatNumber(currentValue);
+    }
+});
+
+function formatNumber(value) {
+    if (value >= 1000) {
+        return (value / 1000).toFixed(1) + 'k';
+    }
+    return value.toString();
 }
 
-function clamp(value, min, max) {
-    return Math.min(Math.max(value, min), max);
+// Simulate sending API update
+function sendApiUpdate(value) {
+    console.log('Sending API update:', value);
+    // Replace this with your actual API call
 }
