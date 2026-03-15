ac14c6793584c31277620b8befe30e3379c275a7
Kimmix <kimmix05@gmail.com>
2026-03-15 19:45:48 +0700

    style

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ function toggleBLECharacteristics() {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width: 16px; height: 16px; margin-right: 6px;">
                 <path d="M6 9l6 6 6-6"/>
             </svg>
-            Hide BLE Characteristics
+            Hide Device Details
         `;
 
         // Scroll to bottom after a short delay to allow rendering
@@ -203,7 +203,7 @@ function toggleBLECharacteristics() {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width: 16px; height: 16px; margin-right: 6px;">
                 <path d="M6 9l6 6 6-6"/>
             </svg>
-            Show BLE Characteristics
+            Show Device Details
         `;
     }
 
